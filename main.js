f2067030c678d0fe6e51a25fdcf4f26abd37aada
samuelweekes <samueljweekes@gmail.com>
2019-02-23 19:38:58 +0000

    Added menu functionality and refactored

--- main.js
+++ main.js
@@ -1,107 +1,131 @@
-// Modules to control application life and create native browser window
-const { app, Menu, Tray, BrowserWindow, ipcMain, ClientRequest } = require('electron');
-const url = require('url');
-const request = require('request');
-const fs = require('fs');
-const Config = require('./config.js');
-const wallpaper = require('wallpaper');
-const ipc = require('electron').ipcMain;
-
-ipc.on('search', function(event, data){
-    searchTerm = data;
-});
-
+const {app, dialog, Menu, Tray, BrowserWindow, globalShortcut, ipcMain} = require('electron');
+const {config} = require('./config.js');
+const {CONSTANTS} = require('./constants.js');
 let tray = null;
-let searchTerm = 'bear';
+let pathWin = null;
+let searchWin = null;
+let contextMenu = null;
+
 app.on('ready', () => {
-  tray = new Tray('./icons/icon.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'New Wallpaper', click: function () { getToken(); } },
-    { label: 'New Search', click: function() { setSearch(); } }
-  ])
-  tray.setToolTip('Bear-paper!');
-  tray.setContextMenu(contextMenu);
+    globalShortcut.register('Option+Cmd+p', () => {
+        console.log('Changing wallpaper...');
+    });
+    searchWin = new BrowserWindow({
+        width: 170,
+        height: 50, 
+        show: false, 
+        frame: false, 
+        nodeIntegration: true
+    });
+    searchWin.on('close', () => {searchWin.hide(); searchWin = null;});
+    pathWin = new BrowserWindow({
+        width: 170, 
+        height: 50, 
+        show: false, 
+        frame: false, 
+        nodeIntegration: true
+    });
+    pathWin.on('close', () => {pathWin.hide(); pathWin = null;});
+    initMenu();
 });
 
-function setSearch() {
- let searchWin;
- searchWin = new BrowserWindow({ width: 180, height: 70, 'node-integration': true });
- searchWin.loadFile('index.html');
+ipcMain.on('search', (event, data) => {
+    searchWin.hide();
+    config.setConfig({key: 'SEARCH', value: data});
+    dialog.showErrorBox('Information', 'Setting Saved!')
+});
+
+ipcMain.on('path', (event, data) => {
+    pathWin.hide();
+    config.setConfig({key: 'PATH', value: data});
+    dialog.showErrorBox('Information', 'Setting Saved!')
+});
+
+async function setToggle(key) {
+    let configItem = await config.readConfig();
+    return configItem[key] === false ? true : false;
 }
 
-function search(token) {
-        console.log('here ' + token);
-      searchUrl = new URL('/search/photos', 'https://api.unsplash.com/')
-      searchUrl.searchParams.append('query', searchTerm);
-      searchUrl.searchParams.append('per_page', '100');
-      request.get({
-        headers: { 'Authorization': 'Bearer ' + token },
-        url: searchUrl
-      }, function (error, response, body) {
-        console.log('Grabbing wallpaper...');
-        image = JSON.parse(body);
-        rand = Math.floor((Math.random() * (image.total/image.total_pages) + 1)); 
-        download(image.results[rand].links.download, './downloads/' + image.results[rand].id, () => {
-           setWallpaper('./downloads/' + image.results[rand].id);
-        });
-      });
-    };
-
-function setWallpaper(image) {
-    wallpaper.set(image);
+async function initMenu() {
+        const tray = new Tray('./icons/icon.png');
+        tray.setToolTip('Splash-Paper!');
+        let menuTemplate = [
+            {
+                label: 'Get Wallpaper',
+                click: () => {console.log('Getting wallpaper...');},
+            },
+            {
+                label: 'Set Search',
+                click: () => {
+                    if(!searchWin) {
+                        searchWin = new BrowserWindow({
+                            width: 170,
+                            height: 50,
+                            show: false, 
+                            frame: false,
+                            nodeIntegration: true
+                        });
+                    }
+                    searchWin.loadFile('search.html'); 
+                    searchWin.show();
+                }
+            },
+            {
+                label: 'Set Timer',
+                submenu: [
+                    {
+                        label: 'Hourly',
+                        click: () => {config.setConfig({key: 'DELAY', value: CONSTANTS.HOUR})},
+                        type: 'radio'
+                    },
+                    {
+                        label: '12 Hours',
+                        click: () => {config.setConfig({key: 'DELAY', value: CONSTANTS.HALFDAY})},
+                        type: 'radio'
+                    },
+                    {
+                        label: 'Daily',
+                        click: () => {config.setConfig({key: 'DELAY', value: CONSTANTS.FULLDAY})},
+                        type: 'radio'
+                    },
+                    {
+                        label: 'Weekly',
+                        click: () => {config.setConfig({key: 'DELAY', value: CONSTANTS.WEEK})},
+                        type: 'radio'
+                    }
+                ]
+            },
+            {
+                label: 'Set Download',
+                submenu: [
+                    {
+                        label: 'Path',
+                        click: () => {
+                            if(!pathWin) {
+                                pathWin = new BrowserWindow({
+                                    width: 170,
+                                    height: 50,
+                                    show: false,
+                                    frame: false,
+                                    nodeIntegration: true
+                                });
+                            }
+                            pathWin.loadFile('path.html'); 
+                            pathWin.show();
+                        }
+                    },
+                    {
+                        label: 'Overwrite',
+                        checked: await config.readConfig('OVERWRITE'), 
+                        click: async () => {
+                            let toggle = await setToggle('OVERWRITE');
+                            console.log(toggle);
+                            config.setConfig({key: 'OVERWRITE', value: toggle})},
+                        type: 'checkbox'
+                    },
+                ]
+            },
+        ];
+    contextMenu = Menu.buildFromTemplate(menuTemplate);
+    tray.setContextMenu(contextMenu);
 }
-
-function getToken() {
-    let path = './config/token.txt';
-
-    if(!fs.existsSync(path)) { 
-        login(); 
-        return;
-    }
-    
-    fs.readFile('./config/token.txt', (err, token) => {
-        if(err) throw err;
-        search(token);
-    });    
-}
-
-function login() {
-  const authUrl = new URL('/oauth/authorize', 'https://unsplash.com');
-  authUrl.searchParams.append('client_id', Config.CLIENT_ID);
-  authUrl.searchParams.append('redirect_uri', Config.REDIRECT_URI);
-  authUrl.searchParams.append('response_type', 'code');
-  authUrl.searchParams.append('scope', 'public');
-  win = new BrowserWindow({ width: 800, height: 600, show: false, 'node-integration': false });
-  win.loadURL(authUrl.href);
-  win.show();
-  win.webContents.on('will-redirect', function (event, newUrl) {
-    let redirectUrl = new URL(newUrl);
-    let code = redirectUrl.searchParams.get('code');
-    const postUrl = new URL('/oauth/token', 'https://unsplash.com');
-    postUrl.searchParams.append('client_id', Config.CLIENT_ID);
-    postUrl.searchParams.append('client_secret', Config.SECRET_KEY);
-    postUrl.searchParams.append('redirect_uri', Config.REDIRECT_URI);
-    postUrl.searchParams.append('code', code);
-    postUrl.searchParams.append('grant_type', "authorization_code");
-    
-    request.post({
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      url: postUrl,
-    }, function (error, response, body) {
-      json = JSON.parse(body);
-      token = json.access_token;
-        fs.writeFile('./config/token.txt', token, (err) => {
-            if(err) throw err;
-            console.log('Token saved');
-            search(token);
-        });
-      });
-    });
- return false; 
-}
-
-function download(uri, filename, callback){
-   request.head(uri, function(err, res, body){
-     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-   });
- }
--- renderer.js
+++ renderer.js
@@ -1,7 +1,28 @@
-//  const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require('electron');
 
-//ipcRenderer.on('code', (event, args) => {
-//    let code = document.getElementByTagName('code')[0];
-//    event.sender.send(code, args); 
-//code.innerHTML; 
-//});    
+if(document.getElementsByTagName('code') !== null) {
+    const code = document.getElementsByTagName('code')[0];
+}
+
+ipcRenderer.on('code', (event, args) => {
+    event.sender.send(code, args); 
+    code.innerHTML; 
+});    
+
+if(document.querySelector('#search') !== null) {
+    document.querySelector('#search').focus();
+    document.querySelector('#search').addEventListener('keydown', function(e) {
+        if(e.keyCode == 13) {
+            ipcRenderer.send('search', this.value);
+        }
+    });
+}
+
+if(document.querySelector('#path') !== null) {
+    document.querySelector('#path').focus();
+    document.querySelector('#path').addEventListener('keydown', function(e) {
+        if(e.keyCode == 13) {
+            ipcRenderer.send('path', this.value);
+        }
+    });
+}
